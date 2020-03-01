1fbe55203a0e936b1113e2a0e6aec804ab8b8ae5
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-03-01 17:49:27 +0000

    again

---
+++ task-tracker-app/src/containers/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../sample_logo.png";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img className="logo" src={logo} />
+    </div>
+  );
+}
+
+export default Nav;
