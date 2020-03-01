c497099a2ae16f39d1a10aca7d141b0c76171987
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-03-01 22:51:47 +0000

    add create card function

--- task-tracker-app/src/containers/MainContainer.js
+++ task-tracker-app/src/containers/MainContainer.js
@@ -1,7 +1,37 @@
 import React from "react";
+import CreateCard from "../components/CreateCard";
 
-function MainContainer() {
-  return <div className="main-container"></div>;
+class MainContainer extends React.Component {
+  state = {
+    cards: []
+  };
+
+  createNewCard = input => {
+    fetch("http://localhost:3000/cards", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        title: input
+      })
+    })
+      .then(resp => resp.json())
+      .then(newCard => {
+        this.setState({
+          cards: [...this.state.cards, newCard]
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="main-container">
+        <CreateCard createNewCard={this.createNewCard} />
+      </div>
+    );
+  }
 }
 
 export default MainContainer;
